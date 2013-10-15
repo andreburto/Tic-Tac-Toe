d800f958acb3066cddd69c88955b0808ae0e8d4b
Andrew Burton <tuglyraisin@gmail.com>
2013-10-15 15:23:19 -0500

    Got the X's and O's interface working.

--- tictactoe/MainWindow.xaml.cs
+++ tictactoe/MainWindow.xaml.cs
@@ -20,9 +20,79 @@ namespace tictactoe
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string xoro = "X";
+        private int square = 0;
+        private bool[] setornot = new bool[9];
+
         public MainWindow()
         {
             InitializeComponent();
+            this.Title = xoro + "'s turn";
         }
+
+        public void xo(Label sqr, int square)
+        {
+            if (setornot[square] == true) { return; }
+            setornot[square] = true;
+
+            if (xoro == "X")
+            {
+                sqr.Content = new tictactoe.Xes();
+                xoro = "O";
+            }
+            else
+            {
+                sqr.Content = new tictactoe.Oes();
+                xoro = "X";
+            }
+
+            this.Title = xoro + "'s turn";
+        }
+        private void lblSqrAA_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            xo((Label)sender, 0);
+        }
+
+        private void lblSqrAB_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            xo((Label)sender, 1);
+        }
+
+        private void lblSqrAC_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            xo((Label)sender, 2);
+        }
+
+        private void lblSqrBA_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            xo((Label)sender, 3);
+        }
+
+        private void lblSqrBB_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            xo((Label)sender, 4);
+        }
+
+        private void lblSqrBC_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            xo((Label)sender, 5);
+        }
+
+        private void lblSqrCA_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            xo((Label)sender, 6);
+        }
+
+        private void lblSqrCB_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            xo((Label)sender, 7);
+        }
+
+        private void lblSqrCC_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            xo((Label)sender, 8);
+        }
+
+
     }
 }
